24ddc7f7727260bac5d1bf59197fab39b5c4a5a0
ChiuYiKai <t106590008@ntut.org.tw>
2020-04-10 11:26:05 +0800

    committee update

--- controller/committee/committeeDashboard.js
+++ controller/committee/committeeDashboard.js
@@ -1,9 +1,11 @@
+var cookieData;
+
 function creatTable(){
     //這個函式的引數可以是從後臺傳過來的也可以是從其他任何地方傳過來的
     //這裡我假設這個data是一個長度為5的字串陣列 我要把他放在表格的一行裡面，分成五列
     var tablegrup="<tr><td><h4>送出日期</h4></td><td><h4>審查期限</h4></td><td><h4>類型</h4></td><td><h4>主題</h4></td><td><h4>審查狀態</h4></td></tr>";
     var tableData="";
-    var cookieData =JSON.parse($.cookie('committeeCookie'));
+    cookieData =JSON.parse($.cookie('committeeCookie'));
     $.cookie('committeeCookie',"");
     $.post('/committee/dashboard',{
         email : cookieData.Email,
@@ -20,13 +22,33 @@ function creatTable(){
                 if(testform[i].isPass)
                     tableData+="<td><h4>finish<h4></td>";
                 else
-                    tableData+="<td><a  class = 'btn btn-info' href ='./review'>click!</td>";
+                    tableData+="<td><a class = 'btn btn-info' href ='./review' onclick='aclick(this)' id ="+i+">click!</td>";
                 tableData+="</tr></form>";
                 tablegrup+=tableData;
 
             }
             $("#tbody1").html(tablegrup);
    });
-   
-    //現在tableData已經生成好了，把他賦值給上面的tbody
+}
+function aclick(obj){
+    $.post('/committee/dashboard',{
+        email : cookieData.Email,
+        password : cookieData.Password},
+        function(data){
+            var testform = data.data.needtestform[obj.id];
+            var dt = JSON.stringify({
+                _OID : testform._id,
+                Email:cookieData.Email ,
+                Password : cookieData.Password,
+                FormOid : testform.formOid ,
+                FormType : testform.fromType ,
+                index:obj.id, 
+                studyandData : testform.StudyandData,
+                Marketassessment : testform.Marketassessment,
+                ManufacturingEvaluation : testform.ManufacturingEvaluation,
+                FinancialEvaluation:testform.FinancialEvaluation,
+                opinion: testform.opinion,
+                isSubmit: testform.isSubmit})
+            $.cookie("committeeCookie",dt)
+        });
 }
