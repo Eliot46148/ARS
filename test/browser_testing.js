5fae137e8abadced00d79dcbf94302913725a8c7
eliot_cheng <eliot_cheng@gss.com.tw>
2020-07-27 22:27:26 +0800

    added test comment

--- test/browser_testing.js
+++ test/browser_testing.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = 'testing';
 process.env.LOG_LEVEL = 'silent';
 
 const app = require("../app.js");
-console.log = function() {}
+console.log = function () { }
 
 require('chromedriver');
 const { webdriver, Builder, By, Key, until } = require('selenium-webdriver');
@@ -11,8 +11,8 @@ const { expect } = require('chai');
 const mongoose = require('mongoose');
 
 let driver;
-    
-describe('教職員入口', function(){
+
+describe('教職員入口', function () {
     const driver = new Builder().forBrowser('chrome').build();
 
     beforeEach(async () => {
@@ -22,7 +22,7 @@ describe('教職員入口', function(){
     it('填寫表單', async () => {
         await driver.get(`localhost:${process.env.PORT}`);
         await driver.findElement(By.xpath('/html/body/div[2]/div[1]/a/div/div')).click();
-        
+
         await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div/a[1]')).click();
         await driver.sleep(1000);
         await driver.findElement(By.xpath('//*[@id="createNewFormNum"]')).sendKeys('123');
@@ -34,11 +34,11 @@ describe('教職員入口', function(){
         expect(html).to.contains('表單填寫');
     });
 
-    describe('進度查詢', function(){
+    describe('進度查詢', function () {
         it('暫存中', async () => {
             await driver.get(`localhost:${process.env.PORT}`);
             await driver.findElement(By.xpath('/html/body/div[2]/div[1]/a/div/div')).click();
-            
+
             await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div/a[1]')).click();
             await driver.sleep(1000);
             // 填寫教師ID
@@ -53,13 +53,13 @@ describe('教職員入口', function(){
 
             // 教師入口
             await driver.findElement(By.xpath('/html/body/div[2]/div[1]/a/div/div')).click();
-            
+
             await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div/a[4]')).click();
             await driver.sleep(1000);
             await driver.findElement(By.xpath('//*[@id="progressNum"]')).sendKeys('123');
             await driver.findElement(By.xpath('/html/body/div[4]/div/div/div[3]/button[2]')).click();
             await driver.sleep(1000);
-            await driver.switchTo().alert().then(() => driver.switchTo().alert().accept(), () => {});
+            await driver.switchTo().alert().then(() => driver.switchTo().alert().accept(), () => { });
 
             const html = await driver.getPageSource();
 
@@ -67,20 +67,20 @@ describe('教職員入口', function(){
         });
         it('查詢錯誤', async () => {
             await driver.get(`localhost:${process.env.PORT}`);
-            await driver.findElement(By.xpath('/html/body/div[2]/div[1]/a/div/div')).click();
-            
-            await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div/a[4]')).click();
+            await driver.findElement(By.xpath('/html/body/div[2]/div[1]/a/div/div')).click(); // 教職員入口頁面
+
+            await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div/a[4]')).click();  // 進度查詢
             await driver.sleep(1000);
             await driver.findElement(By.xpath('//*[@id="progressNum"]')).sendKeys('123');
             await driver.findElement(By.xpath('/html/body/div[4]/div/div/div[3]/button[2]')).click();
             await driver.sleep(1000);
-            await driver.switchTo().alert().then(() => driver.switchTo().alert().accept(), () => {throw Error('沒有pop up')});
+            await driver.switchTo().alert().then(() => driver.switchTo().alert().accept(), () => { throw Error('沒有pop up') });
         });
     });
 
     after(() => {
-        mongoose.connection.db.dropDatabase().then(()=>{
-            driver.quit().then(()=>process.exit());
+        mongoose.connection.db.dropDatabase().then(() => {
+            driver.quit().then(() => process.exit());
         });
     });
 });
